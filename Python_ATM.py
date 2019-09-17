9c3d8df948160494b0db92c094513df371d99516
pradeep <46217896+pradeeppariyar@users.noreply.github.com>
2019-09-17 17:15:16 +0530

    Python_Atm_basic

---
+++ Python_ATM.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 17:13:13 2019
+
+@author: pradeep
+"""
+
+class Bank_Account :
+       
+    def __init__(self): 
+        self.balance=0
+        print("Hello!!! Welcome to the ATM Machine") 
+  
+    def deposit(self): 
+        amount=float(input("Enter amount to be Deposited: ")) 
+        self.balance += amount 
+        print("\n Amount Deposited:",amount) 
+  
+    def withdraw(self): 
+        amount = float(input("Enter amount to be Withdrawn: ")) 
+        if self.balance>=amount: 
+            self.balance-=amount 
+            print("\n You Withdrew:", amount) 
+        else: 
+            print("\n Insufficient balance  ") 
+  
+    def display(self): 
+        print("\n Net Available Balance=",self.balance) 
+  
+# Driver code 
+   
+# creating an object of class 
+s = Bank_Account() 
+   
+# Calling functions with that class object 
+s.deposit() 
+s.withdraw() 
+s.display() 
